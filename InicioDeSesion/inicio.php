69e604f97488a0e8f6cf4f92130a06c07b8ec950
Acxir8 <henrry.mariaca.vaz@gmail.com>
2023-11-28 19:07:54 -0600

    Se agregó el inicio donde se desplegará la información de la base de datos y permitirá al ususario borrar datos

---
+++ InicioDeSesion/inicio.php
@@ -0,0 +1,53 @@
+<?php
+// se usa el requiere para si psi se necesita el archivo conexion
+require "conexion.php";
+mysqli_set_charset($conexion,'utf8');
+
+
+//genear el query
+$consulta_sql="SELECT * FROM usuarios";
+
+//mandar el query por medio de la conexion y almacenaremos el resultado en una variable
+$resultado = $conexion->query($consulta_sql);
+
+// Retorna el numero de filas del resultado. Si encuentra mas de uno lo usamos para imprimir el resultado en nuestra tabla
+$count = mysqli_num_rows($resultado); 
+ 
+echo "<table border='2' >
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Direccion</th>
+        <th>Telefono</th>
+        <th>Correo Electronico</th>
+        <th>Nombre de usuario</th>
+    
+    </tr>";
+
+if ( $count>0 ){
+    //aqui se pintarian los registro de la DB
+    while( $row = mysqli_fetch_assoc($resultado)  ){
+     echo "<tr>";
+     echo"<td>". $row['id'] ."</td>";
+     echo"<td>". $row['nombre'] ."</td>";
+     echo"<td>". $row['direccion'] ."</td>";
+     echo"<td>". $row['telefono'] ."</td>";
+     echo"<td>". $row['correo'] ."</td>";
+     echo"<td>". $row['nombre_usuario'] ."</td>";
+     echo "</tr>";
+     
+    }
+    echo "</table>";
+
+}else{
+    
+    
+    echo"<h1 style='color:red' >Sin Ningun registro</h1>";
+    }
+
+    echo "<h1><a href='EliminarUsuarios.php'>Elimnar Usuario</a></h1>";
+    echo "<h1><a href='index.php'>Cerrar Sesión</a></h1>";
+
+?>
+
+
